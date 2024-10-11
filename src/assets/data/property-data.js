5813d1e1bfc015730b44a29cbc6a204e3ec1980f
Mahbubur Rahman <mahbubur2421@gmail.com>
2024-10-12 02:07:25 +0600

    added completed project files

---
+++ src/assets/data/property-data.js
@@ -0,0 +1,49 @@
+const propertyInfo = [
+  {
+    id: 1,
+    imgSrc:
+      "https://images.unsplash.com/photo-1601760562234-9814eea6663a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVhbGVzdGF0ZXxlbnwwfHwwfHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60",
+    title: "Modern Design villa",
+    price: "$3000.00",
+    bathroom: "2",
+    bedroom: "3",
+  },
+  {
+    id: 2,
+    imgSrc:
+      "https://images.unsplash.com/photo-1601760562234-9814eea6663a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVhbGVzdGF0ZXxlbnwwfHwwfHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60",
+    title: "Sunset Towers",
+    price: "$250000.00",
+    bathroom: "2",
+    bedroom: "3",
+  },
+  {
+    id: 3,
+    imgSrc:
+      "https://images.unsplash.com/photo-1601760562234-9814eea6663a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVhbGVzdGF0ZXxlbnwwfHwwfHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60",
+    title: "Green Valley Apartments",
+    price: "$320000.00",
+    bathroom: "2",
+    bedroom: "3",
+  },
+  {
+    id: 4,
+    imgSrc:
+      "https://images.unsplash.com/photo-1601760562234-9814eea6663a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVhbGVzdGF0ZXxlbnwwfHwwfHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60",
+    title: "Ocean Breeze Condos",
+    price: "$450000.00",
+    bathroom: "2",
+    bedroom: "3",
+  },
+  {
+    id: 5,
+    imgSrc:
+      "https://images.unsplash.com/photo-1601760562234-9814eea6663a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVhbGVzdGF0ZXxlbnwwfHwwfHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60",
+    title: "City Center Lofts",
+    price: "$600000.00",
+    bathroom: "2",
+    bedroom: "3",
+  },
+];
+
+export default propertyInfo;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,134 @@
+import BLOGDATA from "../assets/data/blog-data";
+import Feature_text, { footer_About_Text } from "../assets/data/link-text-data";
+import Logo from "./ui/logo";
+
+function LinkText(prop) {
+  return (
+    <a
+      href="javascript:void(0)"
+      className="mb-3 inline-block text-base text-gray-7 hover:text-primary"
+    >
+      {prop.text}
+    </a>
+  );
+}
+
+function SocialIcon(prop) {
+  return (
+    <a href={prop.href} className="px-3 text-gray-7 hover:text-white">
+      <img src={prop.icon} alt={prop.altText} />
+    </a>
+  );
+}
+
+function BlogList(prop) {
+  return (
+    <a href="blog-details.html" className="group flex items-center gap-[22px]">
+      <div className="overflow-hidden rounded">
+        <img src={prop.imgSrc} alt="blog" />
+      </div>
+      <span className="max-w-[180px] text-base text-gray-7 group-hover:text-white">
+        {prop.title}
+      </span>
+    </a>
+  );
+}
+
+export default function Footer() {
+  return (
+    <footer className=" relative z-10 bg-[#090E34] pt-20 lg:pt-[100px]">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-4/12 xl:w-3/12">
+            <div className="mb-10 w-full">
+              <a
+                href="javascript:void(0)"
+                className="mb-6 inline-block  max-h-[100px]"
+              >
+                <Logo></Logo>
+              </a>
+              <p className="mb-8 lg:max-w-[270px] text-base text-gray-7">
+                We create digital experiences for brands and companies by using
+                technology.
+              </p>
+              <div className="-mx-3 flex items-center">
+                <SocialIcon
+                  href={"javascript:void(0)"}
+                  icon={"/svg/footer-1.svg"}
+                  altText={"footer-1 svg"}
+                ></SocialIcon>
+                <SocialIcon
+                  href={"javascript:void(0)"}
+                  icon={"/svg/footer-2.svg"}
+                  altText={"footer-2 svg"}
+                ></SocialIcon>
+
+                <SocialIcon
+                  href={"javascript:void(0)"}
+                  icon={"/svg/footer-3.svg"}
+                  altText={"footer-3 svg"}
+                ></SocialIcon>
+                <SocialIcon
+                  href={"javascript:void(0)"}
+                  icon={"/svg/footer-4.svg"}
+                  altText={"footer-4 svg"}
+                ></SocialIcon>
+              </div>
+            </div>
+          </div>
+          <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="mb-9 text-lg font-semibold text-white">
+                About Us
+              </h4>
+              <ul>
+                {footer_About_Text.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <LinkText text={item.title}></LinkText>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-3/12 xl:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="mb-9 text-lg font-semibold text-white">
+                Features
+              </h4>
+              <ul>
+                {Feature_text.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <LinkText text={item.title}></LinkText>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+
+          <div className="w-full px-4 md:w-2/3 lg:w-6/12 xl:w-3/12">
+            <div className="mb-10 w-full">
+              <h4 className="mb-9 text-lg font-semibold text-white">
+                Latest blog
+              </h4>
+              <div className="flex flex-col gap-8">
+                {BLOGDATA.map((item) => {
+                  return (
+                    <BlogList
+                      key={item.id}
+                      imgSrc={item.imgSrc}
+                      title={item.title}
+                    ></BlogList>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
